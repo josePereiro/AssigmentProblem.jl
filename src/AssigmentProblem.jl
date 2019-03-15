c67e5e1086b2ac6a733844435bd37ba2f9b19013
Jose Antonio Pereiro Morejon <jose.pereiro.lab@gmail.com>
2019-03-15 04:12:30 -0400

    Files generated by PkgTemplates

---
+++ src/AssigmentProblem.jl
@@ -0,0 +1,5 @@
+module AssigmentProblem
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AssigmentProblem
+using Test
+
+@testset "AssigmentProblem.jl" begin
+    # Write your own tests here.
+end
