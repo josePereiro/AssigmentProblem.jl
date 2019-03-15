f65938b2247331185914b160f6f452bb9b43343f
Jose Antonio Pereiro Morejon <jose.pereiro.lab@gmail.com>
2019-03-15 06:24:08 -0400

    Trying brute force first!!!

--- src/BruteForce.jl
+++ src/BruteForce.jl
@@ -1,10 +1,18 @@
-module AssigmentProblem
 
 """
 Problema de asignación. (Vanderbei LP, pg. 227). Dado un conjunto S de m personas, un conjunto D de m tareas, y para cada iS,jD un costo cij de asignar la tarea j a la persona i, el problema de asignación consiste en asignar a cada persona una y solo una tarea, de manera tal que se minimize el costo total. Formule este problema como un problema de programación lineal con enteros. Escriba una función para resolverlo, que tome como entrada una matriz de costos cij arbitraria.
 
 """
 
-include("BruteForce.jl")
 
-end # module
+export brute_force;
+function brute_force()#costTable::Matrix)
+	
+	#pcount = size(costTable,1);
+	#tcount = size(costTable,2);
+
+	#for pi in 1:pcount
+	
+	println("Not implemented yet!!!");
+
+	end # brute_force
