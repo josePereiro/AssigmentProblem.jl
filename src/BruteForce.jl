c31dbb89fce33a5b84ad8659e46130ff373879dd
Jose Antonio Pereiro Morejon <jose.pereiro.lab@gmail.com>
2019-03-16 17:46:37 -0400

    get_combinations first implemented, it is not very efficient I think but it works!!!

--- src/BruteForce.jl
+++ src/BruteForce.jl
@@ -6,13 +6,82 @@ Problema de asignación. (Vanderbei LP, pg. 227). Dado un conjunto S de m person
 
 
 export brute_force;
-function brute_force()#costTable::Matrix)
+function brute_force(costTable::Matrix{Real})
 	
-	#pcount = size(costTable,1);
-	#tcount = size(costTable,2);
-
-	#for pi in 1:pcount
 	
-	println("Not implemented yet!!!");
-
+	
 	end # brute_force
+
+function getsub(m::Matrix, rtd::Int, ctd::Int)
+    
+    sub = Matrix{eltype(m)}(undef,size(m) .- 1);
+    
+    for d1 in 1:size(m,1)
+        for d2 in 1:size(m,2)
+            if d1 < rtd
+                 if d2 < ctd
+                   sub[d1,d2] = m[d1,d2]
+                elseif d2 > ctd
+                    sub[d1,d2 - 1] = m[d1,d2]
+                end
+            elseif d1 > rtd
+                if d2 < ctd
+                   sub[d1 - 1,d2] = m[d1,d2]
+                elseif d2 > ctd
+                    sub[d1 - 1,d2 - 1] = m[d1,d2]
+                end
+            end
+        end
+    end
+    
+    return sub
+end
+
+function copyrow(m::Matrix, dr::Int)
+    row = Array{eltype(m)}(undef,size(m,2));
+    for ei in 1:size(m,2) row[ei] = m[dr,ei] end 
+    row
+end
+
+
+rowsum(m::Matrix, r::Int) = sum(m, dims = 2)[r] 
+
+export get_combinations
+
+function get_combinations(m)
+   
+   #Stop statement
+    if size(m,1) == 1
+        return toaa(m);
+    end
+    
+    #Recursion
+    combs = [[]];
+    pop!(combs);
+    for row in 1:size(m,2)
+        subcombs = get_combinations(getsub(m,row,1));
+        pushfirst_toall!(subcombs,m[row,1])
+        push_all!(combs,subcombs);
+    end
+    return combs;
+    
+end
+
+function push_all!(to::AbstractArray, from::AbstractArray)
+    for row in 1:length(from) 
+       push!(to,from[row]) 
+    end
+end
+
+function pushfirst_toall!(aa, v)
+    for row in 1:length(aa) 
+       pushfirst!(aa[row],v) 
+    end
+end
+
+function toaa(m)
+    aa = [[zero(eltype(m))]];
+    pop!(aa);
+    push!(aa,copyrow(m,1));
+    aa
+end
